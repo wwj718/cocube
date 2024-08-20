b90dfd0f080d9b091d2f8bfa1bef0e500f9ef4e4
LiangShuai <sliang23@m.fudan.edu.cn>
2024-08-20 11:00:15 +0800

    Synchronize with MicroBlocks CoCube library version 2.1

--- src/cocube/__init__.py
+++ src/cocube/__init__.py
@@ -8,54 +8,74 @@ class CoCube(MicroblocksClient):
 
     @property
     def battery(self):
-        return int(self.request("Battery Percentage", []))
+        return int(self.request("CoCube Battery Percentage", []))
+
+    @property
+    def on_the_mat(self):
+        return bool(self.request("CoCube On The Mat", []))
+
+    @property
+    def card_id(self):
+        return int(self.request("CoCube Card ID", []))
 
     @property
     def position_x(self):
-        return int(self.request("position_X", []))
+        return int(self.request("CoCube position_X", []))
 
     @property
     def position_y(self):
-        return int(self.request("position_Y", []))
+        return int(self.request("CoCube position_Y", []))
 
-    def set_tft_backlight(self, id=0):
-        self.request("set TFT backlight", [id])
+    @property
+    def position_direction(self):
+        return int(self.request("CoCube position_Direction", []))
 
-    def draw_aruco_arker_on_tft(self, id=0):
-        self.request("draw Aruco Marker on TFT", [id])
+    def set_tft_backlight(self, flag=0):
+        self.request("CoCube set TFT backlight", [flag])
 
-    def control_left_motor(self, speed=50):
-        self.request("ControlLeftMotor", [speed])
+    def draw_aruco_marker_on_tft(self, id=0):
+        self.request("CoCube draw Aruco Marker on TFT", [id])
 
-    def control_right_motor(self, speed=50):
-        self.request("ControlRightMotor", [speed])
+    def draw_apriltag_on_tft(self, id=0):
+        self.request("CoCube draw AprilTag on TFT", [id])
 
-    def move_forward(self, speed=50):
-        self.request("Move Forward", [speed])
+    def move(self, direction="forward", speed=40):
+        self.request("CoCube move", [direction, speed])
 
-    def move_backward(self, speed=50):
-        self.request("Move Backward", [speed])
+    def rotate(self, direction="left", speed=30):
+        self.request("CoCube rotate", [direction, speed])
 
-    def turn_left(self, speed=10):
-        self.request("Turn Left", [speed])
+    def move_millisecs(self, direction="forward", speed=40, duration=1000):
+        # self.request("CoCube move at speed for millisecs", [direction, speed, duration], callType="call", timeout=3+duration/1000)
+        self.request("CoCube move for millisecs", [direction, speed, duration], callType="blocking_call", timeout=100)
 
-    def turn_right(self, speed=10):
-        self.request("Turn Right", [speed])
+    def rotate_millisecs(self, direction="left", speed=30, duration=1000):
+        # self.request("CoCube rotate at speed for millisecs", [direction, speed, duration], callType="call", timeout=3+duration/1000)
+        self.request("CoCube rotate for millisecs", [direction, speed, duration], callType="blocking_call", timeout=100)
 
-    def motor_stop(self):
-        self.request("Motor Stop", [])
+    def move_by_steps(self, direction="forward", speed=40, step=50):
+        self.request("CoCube move by step", [direction, speed, step], callType="blocking_call", timeout=100)
 
-    def motor_break(self):
-        self.request("Motor Break", [])
+    def rotate_by_degree(self, direction="left", speed=30, degree=90):
+        self.request("CoCube rotate by degree", [direction, speed, degree], callType="blocking_call", timeout=100)
 
-    def rotate_to_angle(self, angle=0, speed=10):
-        self.request("Rotate to Angle", [angle, speed])
+    def set_wheel_speed(self, left_speed=40, right_speed=40):
+        self.request("CoCube set wheel", [left_speed, right_speed])
 
-    def rotate_to_target(self, x=10, y=10, speed=10):
-        self.request("Rotate to Target", [x, y, speed])
+    def wheels_stop(self):
+        self.request("CoCube wheels stop", [])
 
-    def move_to_target(self, x=10, y=10, speed=50):
-        self.request("Move to Target X _ Y _ Speed _", [x, y, speed])
+    def wheels_break(self):
+        self.request("CoCube wheels break", [])
+
+    def rotate_to_angle(self, angle=0, speed=30):
+        self.request("CoCube rotate to angle", [angle, speed])
+
+    def rotate_to_target(self, target_x=0, target_y=0, speed=30):
+        self.request("CoCube rotate to target", [target_x, target_y, speed])
+
+    def move_to_target(self, target_x=0, target_y=0, speed=50):
+        self.request("CoCube move to target", [target_x, target_y, speed])
 
     ###### module ########
 
